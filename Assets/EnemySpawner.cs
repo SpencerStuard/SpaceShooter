7d59a6c57c5eccf6d0a270c0674d807b07028365
Kevin Riach <kriach09@gmail.com>
2016-06-04 18:21:59 -0700

    Did Target stuff

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public GameObject target;                // The prefab to be spawned.
+	public float spawnTime = 10f;            // How long between each spawn.
+	private Vector3 spawnPosition;
+
+	// Use this for initialization
+	void Start () 
+	{
+		// Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+		int spawncount = 0;
+		if(spawncount < 10){
+			InvokeRepeating ("Spawn", spawnTime, spawnTime);
+			spawncount++;
+			spawnTime--;
+		}
+
+	}
+
+	void Spawn ()
+	{
+		spawnPosition.x = Random.Range (-17, 17);
+		spawnPosition.y = Random.Range (0, 10);
+		spawnPosition.z = Random.Range (-17, 17);
+
+		GameObject o = Instantiate(target, spawnPosition, Quaternion.identity) as GameObject;
+		o.transform.parent = transform;
+	}
+}
---
+++ Assets/TargetObject.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetObject : MonoBehaviour {
+
+	public GameObject explosion = null;
+	public AudioSource explosionSFX = null;
+
+	public int scoreAddition = 1;
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("Do thing");
+
+		if(other.gameObject.tag == "Laser")
+		{
+			DestroyObject(other.gameObject);
+
+			GameObject.Instantiate (explosion as Object);
+			explosionSFX.Play ();
+			AddScore ();
+
+			DestroyObject (gameObject);
+		}
+	}
+
+	void AddScore (){
+		//add score addition to player controller
+	}
+}
