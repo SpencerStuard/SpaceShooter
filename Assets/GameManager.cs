9244fd71985a63d1b0545291180704a559dedee8
Spencer Stuard <spencerstuard@gmail.com>
2016-06-04 18:46:27 -0700

    UI Stuff

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -3,15 +3,43 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
+	private static GameManager _instance;
+
 	enum GameState{Start, Prewave, PostWave, InWave}; 
 
 	GameState CurrentGameStates = GameState.Start;
 
+	//ENEMY AND WAVE INFO
 	int CurrentWaveNumber = 0;
+	public GameObject EnemySpawner;
+
+	//UI INFO
+	public GameObject UIParent;
+
+	public static GameManager Instance
+	{
+		get
+		{
+			if(_instance == null)
+			{
+				//_instance = new GameObject("GameManager");
+				//_instance.AddComponent<GameManager>();
+			}
+
+			return _instance;
+		}
+	}
+
+
+	void Awake()
+	{
+		_instance = this;
+	}
+
 
 	// Use this for initialization
 	void Start () {
-	
+		EnemySpawner.SetActive(false);
 	}
 	
 	// Update is called once per frame
@@ -19,9 +47,11 @@ public class GameManager : MonoBehaviour {
 	
 	}
 
-	void StartGame ()
+	public void StartGame ()
 	{
 		CurrentWaveNumber = 0;
 		CurrentGameStates = GameState.InWave;
+		EnemySpawner.SetActive(true);
+		UIParent.SetActive(false);
 	}
 }
---
+++ Assets/PlayButtonScr.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayButtonScr : MonoBehaviour {
+
+	void OnTriggerEnter (Collider C)
+	{
+		if(C.transform.tag == "Laser")
+		{
+			GameManager.Instance.StartGame();
+		}
+	}
+}
