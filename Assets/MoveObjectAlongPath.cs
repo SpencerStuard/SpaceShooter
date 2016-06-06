014c6e5fd5376e197c1629efd000fb2633188329
mantalayer <mantalayer@gmail.com>
2016-06-05 19:47:50 -0700

    new scenes and movement

---
+++ Assets/MoveObjectAlongPath.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveObjectAlongPath : MonoBehaviour {
+
+    
+    public Transform[] waypointArray;
+    public float percentsPerSecond = 0.02f; // %2 of the path moved per second
+    public float currentPathPercent = 0.0f; //min 0, max 1
+
+    void Update()
+    {
+        currentPathPercent += percentsPerSecond * Time.deltaTime;
+        iTween.PutOnPath(gameObject, waypointArray, currentPathPercent);
+        if (currentPathPercent > 1)
+        {
+            currentPathPercent = 0;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        //Visual. Not used in movement
+        iTween.DrawPath(waypointArray);
+    }
+}
